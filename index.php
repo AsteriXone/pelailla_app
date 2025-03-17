254bd803e9726717622a0a9fb837ce506c00cbb9
peinver <peinver@hotmail.com>
2025-03-17 20:17:07 +0100

    header y footer nuevos
    Nuevos archivos de header y footer

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    require_once("c://xampp/htdocs/pelailla_app/public/header.php");
+
+?>
+
+
+
+
+
+
+
+
+
+<?php
+    require_once("c://xampp/htdocs/pelailla_app/public/footer.php");
+    
+?>
--- public/footer.php
+++ public/footer.php
@@ -1,6 +1,6 @@
-
-    <div class="container-fluid bg-dark pt-3 pb-2">
-         <p class="small text-center">&copy;| Todos los derechos reservados | 2025</p>
-    </div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="/pelailla_app/asset/js/main.js"></script>
 </body>
 </html>
