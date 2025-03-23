8d60212a2c71fc9c1940e4f1050fb634b08af574
peinver <peinver@hotmail.com>
2025-03-23 13:32:41 +0100

    modificar head.php

--- public/head.php
+++ public/head.php
@@ -1,8 +1,8 @@
-<section class="fondopagina" style="height: 900px;">
+<section class="fondopagina, centrarlogin" style="height: 900px;">
 
-<//div class="row"><br></div>
+<div class="row"><br></div>
 
-<!--<div class="row">
+<div class="row">
     <div class="col-lg-2"></div>
     <div class="col-lg-9 titulo">
         <div class="row">
@@ -13,7 +13,40 @@
     </div>
 </div>
 
-<div class="row"><br></div>-->
+<div class="row"><br></div>
+<div class="row">
+      <div class="col-lg-4"></div>  
+      <div class="col-lg-4">
+
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style="border: 5px solid black; margin-top: -100px;">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="Imagenes\anunciosevilla.png" alt="First slide">
+                </div>
+                <div class="carousel-item"> 
+                    <img class="d-block w-100" src="Imagenes\anunciozaragoza.png" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="Imagenes\anuncioleon.png" alt="Third slide">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+
+    </div>
+</div>
 
      
 </section> 
